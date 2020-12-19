72161d19564b984374cd0beaaac3a0ab9dc94b3e
dragonDScript <agomila96@gmail.com>
2020-12-19 17:11:46 +0100

    Update docs and code

--- aeroconfig.py
+++ aeroconfig.py
@@ -4,3 +4,7 @@ import os
 def default():
     print("Installing modules")
     os.system("pipenv install")
+
+def build():
+    print("Building Aero")
+    os.system("pyinstaller main.py --onefile -n aero")
